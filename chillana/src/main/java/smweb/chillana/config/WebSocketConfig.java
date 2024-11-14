621bedd127690e037340dc426be0c2ebe92e959b
saymenhdg <94260801+saymenhdg@users.noreply.github.com>
2024-11-14 14:38:24 +0800

    Add files via upload

---
+++ chillana/src/main/java/smweb/chillana/config/WebSocketConfig.java
@@ -0,0 +1,9 @@
+package smweb.chillana.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig {
+}
